f0854be27da494e7fc08a4b1bacf1961073ead9f
Ari Porad <ari@ariporad.com>
2020-12-04 16:34:45 -0500

    feat: add flashes, nunjucks

--- src/index.ts
+++ src/index.ts
@@ -2,38 +2,74 @@ import 'dotenv/config';
 import express from 'express';
 import { resolve } from 'path';
 import bodyParser from 'body-parser';
+import nunjucks from 'nunjucks';
 import Model from './model';
 import { isValidURL } from './helpers';
-import { encode } from 'punycode';
+import { stringify } from 'querystring';
 
 const app = express();
 const model = new Model();
+
+app.set('views', resolve(__dirname, '..', 'views'));
+
+nunjucks.configure({
+	express: app,
+	noCache: process.env.NODE_ENV === 'development',
+});
 
 app.use(express.static(resolve(__dirname, '..', 'static')));
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded());
 
+app.get('/', (req, res) => {
+	const locals: any = {
+		hasFlash: false,
+		url: req.query.url || req.query.longlink || '',
+		id: req.query.id || req.query.shortlink || '',
+		email: req.query.email || '',
+		flash: req.query.flash || '',
+		flashType: req.query.flashtype || 'info', // NOTE: query param is all lowercase
+	};
+
+	res.render('./views/index.html', locals);
+});
+
 app.post('/api/form-create', async (req, res) => {
 	let { url, email, id } = req.body;
 
 	if (typeof url !== 'string' || !isValidURL(url, ['http:', 'https:', 'mailto:'])) {
-		res.redirect(`/?fail=true&message=${encodeURIComponent('Invalid URL')}`);
+		res.redirect(
+			'/?' +
+				stringify({
+					flash: 'Invalid URL',
+					flashtype: 'danger',
+				}),
+		);
 		return;
 	} else if (
 		typeof email !== 'string' ||
 		!/^[A-Za-z0-9\.]+@(students\.|alumni\.|faculty\.|staff\.|)olin\.edu$/u.test(email)
 	) {
-		res.redirect(`/?fail=true&message=${encodeURIComponent('Invalid Olin Email')}`);
+		res.redirect(
+			'/?' +
+				stringify({
+					flash: 'Invalid Email',
+					flashtype: 'danger',
+				}),
+		);
 		return;
 	} else if (
 		id &&
 		(typeof id !== 'string' || !(id === '' || /^[a-zA-Z0-9_\-\.]{5,20}$/u.test(id)))
 	) {
 		res.redirect(
-			`/?fail=true&message=${encodeURIComponent(
-				'Invalid Shortlink! Must be 5-20 characters using only letters, numbers, ., _, and -.',
-			)}`,
+			'/?' +
+				stringify({
+					flash:
+						'Invalid Shortlink! Must be 5-20 characters using only letters, numbers, ., _, and -.',
+					flashtype: 'danger',
+				}),
 		);
 		return;
 	}
@@ -42,7 +78,7 @@ app.post('/api/form-create', async (req, res) => {
 
 	const shortlink = await model.createShortlink(email, url, id);
 
-	res.redirect(`/success?id=${encodeURIComponent(shortlink.id)}&url=${encodeURIComponent(url)}`);
+	res.redirect(`/success?${stringify({ id: shortlink.id, url: shortlink.longURL })}`);
 });
 
 app.get('/:shortlink', async (req, res, next) => {
