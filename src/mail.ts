510ef1ed29a7e714becff6b6b7bb55905cede09f
Ari Porad <ari@ariporad.com>
2021-01-10 14:25:23 -0800

    feat: add ability to send real emails

--- src/mail.ts
+++ src/mail.ts
@@ -14,6 +14,7 @@ export type SMTPAccount = {
 	port: number;
 	user: string;
 	pass: string;
+	isTestAccount?: boolean;
 };
 
 async function createTestAccount(): Promise<SMTPAccount> {
@@ -26,55 +27,66 @@ async function createTestAccount(): Promise<SMTPAccount> {
 		port: testAccount.smtp.port,
 		user: testAccount.user,
 		pass: testAccount.pass,
+		isTestAccount: true,
 	};
 }
 
 let defaultMailer: Mailer | null = null;
 
 export default async function getDefaultMailer(): Promise<Mailer> {
-	if (!defaultMailer) defaultMailer = await createMailer();
+	if (!defaultMailer) {
+		let account: SMTPAccount;
+
+		if (process.env.SMTP_USE_TEST_ACCOUNT === 'false') {
+			const host = process.env.SMTP_HOST;
+			const port = parseInt(process.env.SMTP_PORT || 'error!', 10);
+			const user = process.env.SMTP_USER;
+			const pass = process.env.SMTP_PASS;
+
+			if (host && port && user && pass) {
+				account = { host, port, user, pass, isTestAccount: false };
+			} else {
+				throw new Error(
+					'WARNING: $SMTP_USE_TEST_ACCOUNT === "true" but SMTP credentials aren\'t properly set!',
+				);
+			}
+		} else {
+			account = await createTestAccount();
+		}
+
+		defaultMailer = await createMailer(account);
+	}
+
 	return defaultMailer;
 }
 
-export async function createMailer(account?: SMTPAccount): Promise<Mailer> {
-	// This is dumb, but TypeScript somehow can't figure out that `account` would never be undefined
-	// inside `sendMail`.
-	let smtpAccount: SMTPAccount;
-	let isTestAccount = false;
-
-	if (!account) {
-		isTestAccount = true;
-		smtpAccount = await createTestAccount();
-	} else {
-		smtpAccount = account;
-	}
-
+export async function createMailer(account: SMTPAccount): Promise<Mailer> {
 	// create reusable transporter object using the default SMTP transport
 	let transporter = createTransport({
-		host: smtpAccount.host,
-		port: smtpAccount.port,
+		host: account.host,
+		port: account.port,
 		auth: {
-			user: smtpAccount.user,
-			pass: smtpAccount.pass,
+			user: account.user,
+			pass: account.pass,
 		},
 	});
 
 	return {
 		get isTestAccount(): boolean {
-			return isTestAccount;
+			return account.isTestAccount === true;
 		},
 
 		async sendMail(to: string, subject: string, text: string, html?: string): Promise<void> {
 			let info = await transporter.sendMail({
-				from: `"Olin.link" <${smtpAccount.user}>`,
+				from: `"Olin.link" <${account.user}>`,
 				to,
 				subject,
 				text,
 				html,
 			});
 
-			console.log('Sent Email:', info);
-			if (isTestAccount) console.log('View Email:', getTestMessageUrl(info));
+			console.log('Sent Email!');
+			if (account.isTestAccount) console.log('View Email:', getTestMessageUrl(info));
 		},
 	};
 }
